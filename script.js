a818a7838cf1905c7757c2035224f376609cafc6
hoodyny <zatzepin42@yandex.ru>
2022-06-12 15:53:04 +0300

    added home section & optimized for mobile devices

--- script.js
+++
@@ -1,6 +0,0 @@
-let burgerMenu = document.querySelector(".burger-menu")
-let mobList = document.querySelector(".mob-list")
-
-burgerMenu.addEventListener("click", () => {
-    mobList.classList.toggle("appearance")
-})
